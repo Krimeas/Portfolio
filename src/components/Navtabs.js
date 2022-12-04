8ccd13807b0732f1de61b5f36519d2bd115efadd
Krimeas <krimeass@gmail.com>
2022-12-04 15:01:44 -0600

    Added code to pages, worked on app.js and index.js.

--- src/components/Navtabs.js
+++ src/components/Navtabs.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
-// Here we are using object destructuring assignment to pluck off our variables from the props object
-// We assign them to their own variable names
+// use object destructuring assignment to pluck off variables from the props object
+// then assign them to their own variable names
 function NavTabs({ currentPage, handlePageChange }) {
   return (
     <ul className="nav nav-tabs">
@@ -9,8 +9,8 @@ function NavTabs({ currentPage, handlePageChange }) {
         <a
           href="#home"
           onClick={() => handlePageChange('Home')}
-          // This is a conditional (ternary) operator that checks to see if the current page is "Home(about)"
-          // If it is, we set the current page to 'nav-link-active', otherwise we set it to 'nav-link'
+          // Conditional (ternary) operator that checks to see if the current page is "Home(about)"
+          // If it is, set the current page to 'nav-link-active', otherwise set it to 'nav-link'
           className={currentPage === 'Home' ? 'nav-link active' : 'nav-link'}
         >
           Home
@@ -20,7 +20,7 @@ function NavTabs({ currentPage, handlePageChange }) {
         <a
           href="#contact"
           onClick={() => handlePageChange('Contact')}
-          // Check to see if the currentPage is `Contact`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
+          // Check to see if the currentPage is `Contact`, and if so use the active link class from bootstrap. Otherwise, set it to a normal nav-link
           className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
         >
           Contact
@@ -30,7 +30,7 @@ function NavTabs({ currentPage, handlePageChange }) {
         <a
           href="#portfolio"
           onClick={() => handlePageChange('Portfolio')}
-          // Check to see if the currentPage is `Portfolio`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
+          // Check to see if the currentPage is `Portfolio`, and if so use the active link class from bootstrap. Otherwise, set it to a normal nav-link
           className={currentPage === 'Portfolio' ? 'nav-link active' : 'nav-link'}
         >
           Portfolio
@@ -40,7 +40,7 @@ function NavTabs({ currentPage, handlePageChange }) {
         <a
           href="#resume"
           onClick={() => handlePageChange('Resume')}
-          // Check to see if the currentPage is `Resume`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
+          // Check to see if the currentPage is `Resume`, and if so use the active link class from bootstrap. Otherwise, set it to a normal nav-link
           className={currentPage === 'Resume' ? 'nav-link active' : 'nav-link'}
         >
           Resume
--- src/components/PortfolioContainer.js
+++ src/components/PortfolioContainer.js
@@ -1,15 +1,15 @@
 import React, { useState } from 'react';
 import NavTabs from './NavTabs';
-import Home from './pages/Home';
-import About from './pages/Contact';
-import Blog from './pages/Portfolio';
-import Contact from './pages/Resume';
+import Home from '../components/pages/Home';
+import About from '../components/pages/Contact';
+import Blog from '../components/pages/Portfolio';
+import Contact from '../components/pages/Resume';
 
 
 export default function PortfolioContainer() {
   const [currentPage, setCurrentPage] = useState('Home');
 
-  // This method is checking to see what the value of `currentPage` is. Depending on the value of currentPage, we return the corresponding component to render.
+  // checking to see what the value of `currentPage` is. Depending on the value of currentPage, return the corresponding component to render.
   const renderPage = () => {
     if (currentPage === 'Contact') {
       return <Contact />;
@@ -27,9 +27,9 @@ export default function PortfolioContainer() {
 
   return (
     <div>
-      {/* We are passing the currentPage from state and the function to update it */}
+      {/* Passing the currentPage from state and the function to update it */}
       <NavTabs currentPage={currentPage} handlePageChange={handlePageChange} />
-      {/* Here we are calling the renderPage method which will return a component  */}
+      {/* Calling the renderPage method which will return a component  */}
       {renderPage()}
     </div>
   );
